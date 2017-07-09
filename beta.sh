e2f544c1a0308e70b06891bc97f95059ec4e9879
Marq <marq@iki.fi>
2017-07-09 18:30:34 +0000

    Do not include svn files in the zip

--- beta.sh
+++ beta.sh
@@ -4,5 +4,5 @@ rm -r application.* *.c *.bas *.prg *.s *.o *.png *.gif *.d64
 cd ..
 rm petscii-beta.zip
 
-zip -r petscii-beta.zip petscii
+zip -r petscii-beta.zip petscii -x petscii/.svn\*
 scp petscii-beta.zip kameli.net:public_html/kode
